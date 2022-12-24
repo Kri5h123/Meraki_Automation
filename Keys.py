58db0513801fd551609d3795c26976beddb2843e
Kri5h123 <bfear58@gmail.com>
2022-12-24 09:39:00 +0000

    Update Keys.py

--- Keys.py
+++ Keys.py
@@ -2,8 +2,8 @@ dashboard_Key = "" #Enter the MEraki Dashboard API key
 sw_username = '' #Enter the Switch Admin Username 
 sw_passwd = '' #Enter the Switch Admin password 
 enable_pwd = '' #Enter the Switch Enable password 
-Radius_Key = '' #Enter the Radius Key
-Organization_id = '' #Enter the Organization id
-NW_id = '' #Enter the Network Id
+radius_Key = '' #Enter the Radius Key
+organization_id = '' #Enter the Organization id
+nw_id = '' #Enter the Network Id
 
 
